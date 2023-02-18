c3821e6bcd50283f4f89d292155885a1caabe124
Jason Washburn <35488541+jasonwashburn@users.noreply.github.com>
2023-02-18 21:16:52 +0000

    adds devcontainer config

---
+++ .devcontainer/install_exercism.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget https://github.com/exercism/cli/releases/download/v3.1.0/exercism-3.1.0-linux-x86_64.tar.gz -P /tmp/
+tar -xf /tmp/exercism-3.1.0-linux-x86_64.tar.gz --directory $HOME/.local/bin/
